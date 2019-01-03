4fda1977c98904247eb6f8441629f7de3513a6f7
martveeken <mart@inkstrategy.com>
2019-01-03 20:42:00 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now} ${req.method}  ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '/n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintainance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<H1>Hello express</H1>');
+  res.send({
+    name: 'Andrew',
+    likes: [
+      'biking',
+      'cities'
+      ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Hello!'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
